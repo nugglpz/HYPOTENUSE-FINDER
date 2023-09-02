84ffb25cb203dd055031ac6713cdaec84cc65538
nugg <143720686+nugglpz@users.noreply.github.com>
2023-09-02 19:14:04 +0800

    Add files via upload

---
+++ hypofinder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+	public class hypofinder {
+	 
+		public static void main(String[]args) {
+		 
+		 double x;
+		 double y;
+		 double z;
+		 
+		 Scanner scanner  = new Scanner(System.in);
+		 
+		 System.out.println("Enter side x: ");
+		 x = scanner.nextDouble();
+		 System.out.println("Enter side y: ");
+		 y = scanner.nextDouble();
+		 
+		 z = Math.sqrt((x*x)+(y*y));
+		 
+		 System.out.println("The Hypotenuse is " + z);
+		 
+		 scanner.close();
+		 
+	 }
+	}
